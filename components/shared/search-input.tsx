dee921826341db47c427745810c533f90ebea38a
ifaqer <super.mr.remix@gmail.com>
2024-10-21 00:33:17 +0300

    Добавлен поиск + API

--- components/shared/search-input.tsx
+++ components/shared/search-input.tsx
@@ -5,8 +5,10 @@ import { Search } from 'lucide-react';
 import React from 'react';
 import { Input } from '../ui';
 import { cn } from '@/lib/utils';
-import { useClickAway } from 'react-use';
+import { useClickAway, useDebounce } from 'react-use';
 import Link from 'next/link';
+import { Api } from '@/services/api-client';
+import { Product } from '@prisma/client';
 
 interface Props {
   className?: string;
@@ -14,10 +16,21 @@ interface Props {
 
 export const SearchInput: React.FC<Props> = ({ className }) => {
   const ref = React.useRef(null);
+  const [products, setProducts] = React.useState<Product[]>([]);
+  const [searchQuery, setSearchQuery] = React.useState('');
   const [focused, setFocused] = React.useState(false);
   useClickAway(ref, () => {
     setFocused(false);
   });
+  useDebounce(
+    () => {
+      Api.products.search(searchQuery).then((items) => {
+        setProducts(items);
+      });
+    },
+    250,
+    [searchQuery],
+  );
   return (
     <>
       {focused && <div className="fixed top-0 left-0 right-0 bottom-0 bg-black/50 z-30" />}
@@ -28,25 +41,28 @@ export const SearchInput: React.FC<Props> = ({ className }) => {
         <Input
           className="rounded-2xl  w-full bg-gray-100 pl-11"
           type="text"
+          value={searchQuery}
           placeholder="Найти пиццу..."
+          onChange={(e) => setSearchQuery(e.target.value)}
           onFocus={() => setFocused(true)}
         />
-        <div
-          className={cn(
-            'absolute w-full bg-white rounded-2xl p-2 top-14 shadow-md transition-all duration-200 invisible opacity-0 z-30',
-            focused && 'visible opacity-100 top-12',
-          )}>
-          <Link
-            href="/product/1"
-            className=" flex rounded-xl items-center gap-3 w-full px-3 py-2 hover:bg-primary/10">
-            <img
-              className="rounded-sm h-8 w-8"
-              src="https://media.dodostatic.net/image/r:292x292/11EE7D610E8BBB248F31270BE742B4BD.avif"
-              alt="Пицца 1"
-            />
-            <span>Пицца 1</span>
-          </Link>
-        </div>
+        {products.length > 0 && (
+          <div
+            className={cn(
+              'absolute w-full bg-white rounded-2xl p-2 top-14 shadow-md transition-all duration-200 invisible opacity-0 z-30',
+              focused && 'visible opacity-100 top-12',
+            )}>
+            {products.map((product) => (
+              <Link
+                key={product.id}
+                href={`/product/${product.id}`}
+                className=" flex rounded-xl items-center gap-3 w-full px-3 py-2 hover:bg-primary/10">
+                <img className="rounded-sm h-8 w-8" src={product.imageUrl} alt={product.name} />
+                <span>{product.name}</span>
+              </Link>
+            ))}
+          </div>
+        )}
       </div>
     </>
   );
