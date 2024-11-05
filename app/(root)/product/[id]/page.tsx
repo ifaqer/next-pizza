403467615415c721b5257b74fc8d68becc1b413f
ifaqer <super.mr.remix@gmail.com>
2024-11-05 04:50:10 +0300

    fix-modal

--- app/(root)/product/[id]/page.tsx
+++ app/(root)/product/[id]/page.tsx
@@ -14,7 +14,7 @@ export default async function ProductPage({ params: { id } }: { params: { id: st
   return (
     <Container className="flex flex-col my-10">
       <div className="flex flex-1 justify-center">
-        <ProductImage size={20} imageUrl={product.imageUrl} />
+        <ProductImage size={22} imageUrl={product.imageUrl} />
         <div className="w-[490px] bg-[#f7f6f5] p-7">
           <Title text={product.name} size="md" className="font-extrabold mb-1"></Title>
           <p className="text-gray-400">Описание продукта один-два...</p>
